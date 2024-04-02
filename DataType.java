72ac6550d59de521bc00e7926ec653edea46ee85
OreoluwaElizabeth <165695242+OreoluwaElizabeth@users.noreply.github.com>
2024-04-02 13:24:05 -0800

    Added new files

---
+++ DataType.java
@@ -0,0 +1,15 @@
+public class DataType {
+  public static void main(String [] args) {
+ 
+  float value = 5 / 2;
+
+ String dataType = Float.class.getName();
+
+  System.out.println(dataType);
+
+
+}
+
+
+
+}
